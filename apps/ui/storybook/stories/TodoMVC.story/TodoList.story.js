7886b26cb00011cb04907a800105811d2eddb963
Cliff <qm29213039@gmail.com>
2017-11-06 15:58:56 +0800

    (feat): TodoList include all Todoitem

--- apps/ui/storybook/stories/TodoMVC.story/TodoList.story.js
+++ apps/ui/storybook/stories/TodoMVC.story/TodoList.story.js
@@ -10,12 +10,15 @@ const story = storiesOf('TodoMVC/TodoList', module);
 story.add('Test TodoList', () => {
   const props = {
     todos: [{
+      id: 1,
       text: 'Task #1',
       isCompleted: true,
     }, {
+      id: 2,
       text: 'Task #2',
       isCompleted: true,
     }, {
+      id: 3,
       text: 'Task #3',
       isCompleted: false,
     }]
--- modules/components/src/TodoList/component.js
+++ modules/components/src/TodoList/component.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import TodoItem from '../TodoItem';
 
 class TodoList extends Component {
   render(){
+    const { todos } = this.props;
+
     return(
       <View>
-        <Text>test</Text>
+        {
+          todos.map((todo) => {
+            return <TodoItem key={ todo.id } todo={ todo } />
+          })
+        }
       </View>
     );
   }
